2841e31c9b869726169ba4f947dc20dd59c07c5e
João Barboza <jprsbarboza@gmail.com>
2024-06-22 15:05:54 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,67 @@
+let listaNumerosSorteados = [];
+
+let numeroSecreto = gerarNumeroAleatorio();
+
+let numTentativas = 1;
+
+function exibirTextoNaTela(tag, texto) {
+  let campo = document.querySelector(tag);
+  campo.innerHTML = texto;
+}
+
+function mensagemNovoJogo() {
+  exibirTextoNaTela("h1", "Jogo do número secreto");
+  exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
+}
+mensagemNovoJogo();
+console.log(`O numero secreto é ${numeroSecreto}`);
+function verificarChute() {
+  let chute = document.querySelector("input").value;
+  zerarItensLista();
+  if (chute == numeroSecreto) {
+    exibirTextoNaTela("h1", "Acertou!");
+    let palavraTentativa = numTentativas > 1 ? "tentativas" : "tentativa";
+    let mensagemTentativa = `Você acertou o número secreto com ${numTentativas} ${palavraTentativa}!`;
+    exibirTextoNaTela("p", mensagemTentativa);
+    document.getElementById("reiniciar").removeAttribute("disabled");
+  } else {
+    if (chute > numeroSecreto) {
+      exibirTextoNaTela("p", "O numero secreto é menor!");
+    } else {
+      exibirTextoNaTela("p", "O numero secreto é maior!");
+    }
+    numTentativas++;
+    limparCampo();
+  }
+}
+function zerarItensLista() {
+  if (listaNumerosSorteados.length == 4) {
+    listaNumerosSorteados = [];
+  }
+}
+
+function gerarNumeroAleatorio() {
+  let numeroEscolhido = parseInt(Math.random() * 10 + 1);
+  if (listaNumerosSorteados.includes(numeroEscolhido)) {
+    return gerarNumeroAleatorio();
+  } else {
+    listaNumerosSorteados.push(numeroEscolhido);
+    console.log(listaNumerosSorteados);
+    return numeroEscolhido;
+  }
+}
+
+function limparCampo() {
+  chute = document.querySelector("input");
+  chute.value = "";
+}
+
+function novoJogo() {
+  numeroSecreto = gerarNumeroAleatorio();
+  verificarChute();
+  limparCampo();
+  numTentativas = 1;
+  mensagemNovoJogo();
+  console.log(`O numero secreto é ${numeroSecreto}`);
+  document.getElementById("reiniciar").setAttribute("disabled", true);
+}
